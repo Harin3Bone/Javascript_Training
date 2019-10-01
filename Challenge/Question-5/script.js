6538c8c5d8b817bfe05b06e3fcbe7b65b9568e9e
Zchwazer <yamiyomuyumi@gmail.com>
2019-10-01 22:12:43 +0700

    Challenge
    Question in challenge 5

---
+++ Challenge/Question-5/script.js
@@ -0,0 +1,30 @@
+//! Coding Challenge 5
+    //? Question : John and his family went on a holiday and went to 3 different restaurant.  
+    //? Bills price is $124 , $48 , $268 , $180 , $42
+    //? He want to tip waiter/waitress 20% if bill < $50
+    //? He want to tip waiter/waitress 15% if $50 <= bill <= $200
+    //? He want to tip waiter/waitress 10% if bill > $200 
+        //# Condition
+            //* 1.Create an object with an array for the bill values.
+            //* 2.Add method to calculate the tip.
+            //* 3.This method should include a loop to iterate over all the paid bills and dfo the tip calculations.
+            //* 4.As an output create 
+            //todo 4.1) a new array containing all tips
+            //todo 4.2) an array containing final paid amounts (bill + tip)
+                //~ HINT1 : To calculate 20% of value you can simply multiply if with 20/200 = 0.2
+                //~ HINT2 : Start with 2 Empty array [] as properties and then fill them up in the loop
+                //~ KEY   : Use everything you learn in 10 lesson to solve the problem
+    //? EXTRA : Mark family also went on a holiday go different 4 restaurant
+    //? Bills price is $77 , $375 , $110 , $45
+    //? He want to tip waiter/waitress 20% if bill < $100
+    //? He want to tip waiter/waitress 20% if $100 <= bill <= $300
+    //? He want to tip waiter/waitress 20% if bill > $300
+        //# Condition
+            //* 5.Implement the same functionality as before, this time using Mark's tipping rules
+            //* 6.Create a function (not a method) to calculate the average of a given array of tips.
+            //* 7.Calculate the average tip for each family
+            //* 8.Log to the console which family paid the highest
+                //~ HINT : Loop over the array and in each iteration store the current sum in a variable (Starting from 0)
+                //~ After you have the sum of the array, divide it by the number of elements in it (That's how you calculate the average)
+
+//! My Solution

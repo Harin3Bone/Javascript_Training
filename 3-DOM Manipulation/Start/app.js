a7f8b3c0680d382b4a6d9b10503b14a8f4b08360
Harin.Thananam <harin.thananam@gmail.com>
2020-06-28 11:04:50 +0700

    DOM Access

--- 3-DOM Manipulation/Start/app.js
+++ 3-DOM Manipulation/Start/app.js
@@ -8,3 +8,41 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+var scores, roundScore, activePlayer, dice;
+// var dice1
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 1;
+
+//! Random Number Method
+//# 1.Math.random()             -> random number 0 to 1 (with decimal number)
+//* Math.random() * 6 -> random number 0 to 5
+//# 2.Math.floor(Math.random()) -> random number 0 to 1 (with integer number)
+//* if we get 0 + 1 -> 1 actually get 5 then will be 6 (it will not fetch 0 but it can get as well)
+
+dice = Math.floor(Math.random() * 6) + 1;
+// dice1 = Math.floor(Math.random() * 6) + 1;
+
+//! Query Selector Keyword
+//? Use id in html component to put data from Js to HTML
+// document.querySelector('#score-0').textContent = dice0;
+// document.querySelector('#score-1').textContent = dice1;
+
+//! Current Score
+//# <em></em> use for make font italic
+//~ If use textContent you can't use <tag> in value
+//~ If use innerHTML you can use <tag> in value
+
+// document.querySelector('#current-' + activePlayer).textContent = '<em>' + dice + '</em>';
+document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+//! Dice Image
+//? . use for in HTML 'Class'
+//? # use for in HTML 'Id'
+document.querySelector('.dice').style.display = 'none';
+
+//! EVENT Manipulation
+//? Events : Notification that are sent to notify the code that something happened on the webpage
+//? Event listener : A function that performs an action based on a certain event. It waits for a specific event to happen
+ 
